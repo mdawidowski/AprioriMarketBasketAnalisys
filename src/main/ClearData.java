3fbe44973c77dae9e77cd78a25cd528e1a1ef5ad
Marcin Dawidowski <marcin.dawidowskipl@gmail.com>
2018-01-12 20:27:32 +0100

    przygotowywanie danych apriori, gotowa tabela data, wczytywanie danych z pliku

---
+++ src/main/ClearData.java
@@ -0,0 +1,40 @@
+package main;
+import java.io.*;
+public class ClearData{
+// Usuwamy niepotrzebne linie i je liczymy. Wynik zapisujemy do pliku
+public static void przygotuj(){
+   File input = new File("D01.txt");
+   File output = new File("plain.txt");
+   String tekst, all = "";
+   StringBuilder sB = new StringBuilder(all);
+
+   try {
+     try {
+       try {
+         Reader reader;
+           reader = new InputStreamReader(new FileInputStream(input),"ASCII");
+         BufferedReader fin = new BufferedReader(reader);
+         Writer writer = new OutputStreamWriter(new FileOutputStream(output), "UTF-8");
+         BufferedWriter fout = new BufferedWriter(writer);
+         String tekscik;
+         int counter = -1;
+         fin.readLine();
+         tekst = fin.readLine();
+         while(tekst!=null){
+           counter += 1;
+           tekscik = tekst.substring(20,57);
+           sB.append(tekscik);
+           sB.append("\n");
+           tekst = fin.readLine();
+         }
+         all = sB.toString();
+         String string = String.valueOf(counter);
+         fout.write(string + '\n');
+         fout.write(all);
+         fin.close();
+         fout.close();
+       } catch (UnsupportedEncodingException e) {}
+     } catch (FileNotFoundException e) {}
+   } catch (IOException e) {}
+ }
+}
